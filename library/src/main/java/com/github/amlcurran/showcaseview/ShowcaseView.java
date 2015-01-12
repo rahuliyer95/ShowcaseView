3e6e97090709df222823edf8bc3bceedc355e628
anshul1235 <anshul1235@gmail.com>
2015-01-13 01:06:37 +0530

    added feature of setting inner radius and outer radius for two circles in new Showcase view

--- library/src/main/java/com/github/amlcurran/showcaseview/ShowcaseView.java
+++ library/src/main/java/com/github/amlcurran/showcaseview/ShowcaseView.java
@@ -679,4 +679,14 @@ public class ShowcaseView extends RelativeLayout
         }
     };
 
+    @Override
+    public void setInnerCircleRadius(float innerRadius) {
+        showcaseDrawer.setInnerCircleRadius(innerRadius);
+    }
+
+
+    @Override
+    public void setOuterCircleRadius(float outerRadius) {
+        showcaseDrawer.setOuterCircleRadius(outerRadius);
+    }
 }
